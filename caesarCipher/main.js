46d472cef8a117bbbcb66ef63460f39c60008a5c
Khulekani Gcaba <khulekanigcaba1211@gmail.com>
2024-05-31 22:11:50 +0200

    Added the decrypt function

--- caesarCipher/main.js
+++ caesarCipher/main.js
@@ -5,6 +5,10 @@ const submitBtn = document.querySelector('button')
 const cipherText = document.getElementById('cipher-message')
 const cipherBlock = document.querySelector('.cipher')
 
+const decryptionKey = document.getElementById('decipher-keys')
+const decipherBtn = document.getElementById('decrypt-btn')
+const message2 = document.getElementById('Textarea2')
+
 const cipherButtons = document.querySelectorAll('input[name="btnradio"]');
 const cipherOptions = document.querySelectorAll('.encrypt')
 
@@ -21,6 +25,7 @@ cipherButtons.forEach(cipherButton => {
   })
 })
 
+//Assigning an Encryption Key
 let key;
 
 encryptionKey.addEventListener('change', () => {
@@ -28,7 +33,7 @@ encryptionKey.addEventListener('change', () => {
   //console.log(key)
 })
 
-
+//Encrypting the text
 submitBtn.addEventListener('click', () => {
   const plaintext = message.value
   
@@ -65,3 +70,48 @@ function CaesarCipherEncrypt(message, key){
 }
 
 //console.log(CaesarCipherEncrypt('KHULEKANI', 9))
+
+//Decryption Section of JavaScript
+let decryptKey;
+
+decryptionKey.addEventListener('change', () => {
+  decryptKey = decryptionKey.value;
+  //console.log(decryptKey)
+})
+
+//Encrypting the text
+decipherBtn.addEventListener('click', () => {
+  const plaintext = message2.value
+  
+  const decrypt_key = parseInt(decryptKey)
+
+  const decipher = CaesarCipherDecrypt(plaintext.toUpperCase(), decrypt_key)
+  
+  cipherText.innerHTML = decipher
+  cipherBlock.style.display = "block";
+})
+
+//Decryption function
+function CaesarCipherDecrypt(message, key){
+  //Letters of the alphabet in CAPS
+  const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  
+  //Array to store the encrypted characters
+  let encrypt = []
+
+  //regex matching the letters of the english alphabet
+  const regex = /[A-Z]/
+
+  for(let i = 0; i < message.length; i++){
+
+      if(regex.test(message[i])){
+          let index = alphabets.indexOf(message[i])
+          let shiftedIndex = (index - key + 26) % 26
+          encrypt.push(alphabets[shiftedIndex])
+      }else{
+          encrypt.push(message[i])
+      }
+  }
+  //console.log(encrypt)
+  return encrypt.join("")
+}
